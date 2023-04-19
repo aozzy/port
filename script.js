cfdf07437f68189facb76796a9c151ce498474d4
aosman <pianogreen0@gmail.com>
2023-04-19 17:37:23 +0100

    adding nav

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const tween = KUTE.fromTo(
 )
 
 tween.start()
+
+const togglebtn = document.getElementsByClassName('toggle-button')[0];
+const navbarlinks =document.getElementsByClassName('navbar-links')[0];
+
+const topOfNav = navbarlinks.offsetTop;
+togglebtn.addEventListener('click',() =>{
+  
+  navbarlinks.classList.toggle('active')
+  
+  
+});
